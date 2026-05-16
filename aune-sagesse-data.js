a7b104aae0f6d9120b4835384f389923db933f2f
Ramikaze <rami.dakhlallah@gmail.com>
2026-05-16 16:45:31 +0100

    Update aune-sagesse-data.js

--- aune-sagesse-data.js
+++ aune-sagesse-data.js
@@ -125,6 +125,283 @@ const AUNE_SAGESSE = [
       },
       {
         "title": "31 - L'interdiction de certaines fraternités"
+      },
+      {
+        "title": "32 - Préserver les anciennes fraternités"
+      },
+      {
+        "title": "33 - La fraternité authentique"
+      },
+      {
+        "title": "34 - Le choix d'un frère"
+      },
+      {
+        "title": "35 - Tolérer le faux pas du frère"
+      },
+      {
+        "title": "36 - Les meilleurs frères"
+      },
+      {
+        "title": "37 - Les pires frères"
+      },
+      {
+        "title": "38 - Tester les frères"
+      },
+      {
+        "title": "39 - Conseiller les frères"
+      },
+      {
+        "title": "40 - Honorer les frères et les louer"
+      },
+      {
+        "title": "41 - Satisfaire les besoins des frères"
+      },
+      {
+        "title": "42 - Le bon comportement entre frères"
+      }
+    ]
+  },
+  {
+    "title": "6 - LA POLITESSE",
+    "subparts": [
+      {
+        "title": "43 - La vertu de la politesse"
+      },
+      {
+        "title": "44 - La politesse et la raison"
+      },
+      {
+        "title": "45 - L'éducation de l'âme"
+      },
+      {
+        "title": "46 - Les principes de la politesse"
+      },
+      {
+        "title": "47 - Les effets de la politesse"
+      },
+      {
+        "title": "48 - La signification de la politesse"
+      },
+      {
+        "title": "49 - La meilleure des bonnes manières"
+      },
+      {
+        "title": "50 - L'incitation à bien éduquer la famille"
+      },
+      {
+        "title": "51 - La manière d'éduquer"
+      },
+      {
+        "title": "52 - Ce qu'il faut respecter dans l'éducation"
+      },
+      {
+        "title": "53 - S'éduquer par l'éducation d'Allah"
+      },
+      {
+        "title": "54 - L'éducation d'Allah"
+      }
+    ]
+  },
+  {
+    "title": "7 - L'APPEL À LA PRIÈRE (ADHĀN)",
+    "subparts": [
+      {
+        "title": "55 - La vertu de l'adhān"
+      },
+      {
+        "title": "56 - Le muezzin"
+      },
+      {
+        "title": "57 - Réciter l'appel à la prière dans l'oreille"
+      }
+    ]
+  },
+  {
+    "title": "8 - LE TOURMENT",
+    "subparts": [
+      {
+        "title": "58 - Eviter de tourmenter les gens"
+      },
+      {
+        "title": "59 - Eviter de tourmenter quiconque même les petites fourmis"
+      },
+      {
+        "title": "60 - Tourmenter un croyant"
+      }
+    ]
+  },
+  {
+    "title": "9 - LE PRISONNIER [DE GUERRE]",
+    "subparts": [
+      {
+        "title": "61 - Il n'est pas autorisé de se constituer prisonnier"
+      },
+      {
+        "title": "62 - La bienfaisance envers le prisonnier"
+      }
+    ]
+  },
+  {
+    "title": "10 - LA NOURRITURE",
+    "subparts": [
+      {
+        "title": "63 - L'incitation à manger peu"
+      },
+      {
+        "title": "64 - Mise en garde contre l'excès dans la nourriture"
+      },
+      {
+        "title": "65 - Parmi les vices de la gloutonnerie"
+      },
+      {
+        "title": "66 - La faim"
+      },
+      {
+        "title": "67 - La mesure de la nourriture"
+      },
+      {
+        "title": "68 - Le bon comportement pendant le repas"
+      }
+    ]
+  },
+  {
+    "title": "11 - L'UNION DES CŒURS",
+    "subparts": [
+      {
+        "title": "69 - Incitation à l'union des cœurs"
+      },
+      {
+        "title": "70 - Nul bien en celui qui n'établit pas de liens d'affection et avec qui on n'établit pas de liens d'affection"
+      }
+    ]
+  },
+  {
+    "title": "12 - ALLAH",
+    "subparts": [
+      {
+        "title": "71 - La signification de «Allah»"
+      }
+    ]
+  },
+  {
+    "title": "13 - LE GOUVERNEMENT",
+    "subparts": [
+      {
+        "title": "72 - La nécessité d'un gouvernement"
+      },
+      {
+        "title": "73 - Le gouvernement des malfaisants"
+      },
+      {
+        "title": "74 - La valeur du gouvernement"
+      }
+    ]
+  },
+  {
+    "title": "14 - L'ESPOIR",
+    "subparts": [
+      {
+        "title": "75 - L'espoir est une miséricorde"
+      },
+      {
+        "title": "76 - L'espoir ne cesse jamais"
+      },
+      {
+        "title": "77 - Mise en garde contre les faux espoirs"
+      },
+      {
+        "title": "78 - L'espoir et l'échéance de la vie"
+      },
+      {
+        "title": "79 - Les résultats des grands espoirs"
+      },
+      {
+        "title": "80 - Les espoirs réduits"
+      },
+      {
+        "title": "81 - L'interdiction de fonder ses espoirs sur un autre qu'Allah"
+      }
+    ]
+  },
+  {
+    "title": "15 - LA COMMUNAUTÉ [MUSULMANE]",
+    "subparts": [
+      {
+        "title": "82 - Le statut de la communauté musulmane"
+      },
+      {
+        "title": "83 - Les meilleurs de la communauté musulmane"
+      },
+      {
+        "title": "84 - La communauté du juste milieu"
+      },
+      {
+        "title": "85 - Ce qui suscite le bien de la communauté musulmane"
+      },
+      {
+        "title": "86 - Le statut de la communauté musulmane dans l'Au-delà"
+      },
+      {
+        "title": "87 - Ce qui enlève la splendeur de la communauté musulmane"
+      },
+      {
+        "title": "88 - Ce que le Prophète (s) craint pour sa communauté"
+      }
+    ]
+  },
+  {
+    "title": "16 - L'IMĀMAT (1) - L'IMĀMAT GÉNÉRAL",
+    "subparts": [
+      {
+        "title": "89 - L'importance de l'Imāmat"
+      },
+      {
+        "title": "90 - La supériorité de l'Imāmat vis-à-vis de la prophétie"
+      },
+      {
+        "title": "91 - La nécessité d'un Imām"
+      },
+      {
+        "title": "92 - La Preuve (al-ḥujjat) est un Imām connu"
+      },
+      {
+        "title": "93 - L'Imām peut être apeuré et caché"
+      },
+      {
+        "title": "94 - Sans la présence de l'Imām, la terre serait engloutie"
+      },
+      {
+        "title": "95 - L'appel de chaque nation par son chef (Imām)"
+      },
+      {
+        "title": "96 - L'importance de la connaissance de l'Imām"
+      },
+      {
+        "title": "97 - Les conditions de l'Imāmat et les caractéristiques de l'Imām"
+      },
+      {
+        "title": "98 - Ce qui a été ordonné aux Imāms justes"
+      },
+      {
+        "title": "99 - Les droits et devoirs réciproques entre l'Imām et la communauté"
+      },
+      {
+        "title": "100 - Vos Imāms sont vos délégués"
+      },
+      {
+        "title": "101 - Celui qui accepte la gouvernance d'un Imam illégitime"
+      },
+      {
+        "title": "102 - Les dirigeants de l'Enfer"
+      },
+      {
+        "title": "103 - Les faux prétendants à l'Imāmat"
+      },
+      {
+        "title": "104 - Nulle obéissance à celui qui n'obéit pas à Allah"
+      },
+      {
+        "title": "105 - L'obligation de se révolter contre les dirigeants tyranniques"
       }
     ]
   }
